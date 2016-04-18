83f6bcba569b85be95451d27117814c0435b11e3
Martin Ben <mebenav88@gmail.com>
2016-04-17 21:27:38 -0400

    add crud to post controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,2 +1,57 @@
 class Api::PostsController < ApplicationController
+
+  before_action :find_post, only: [:show, :update, :destroy]
+
+  def index
+    render json: Post.all.order("id DESC")
+  end
+
+  def show
+    render json: @post
+  end
+
+  def create
+    @post = Post.create(post_params)
+
+    if @post.save
+      render status: 200, json: {
+        message: "succesfully created a post",
+        post: @post
+      }.to_json
+    else
+      render status: 422, json: {
+        message: @post.errors
+      }.to_json
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      render status: 200, json: {
+        message: "succesfully updated the post",
+        post: @post
+      }.to_json
+    else
+      render status: 200, json: {
+        message: "failed to update post",
+        post: @post
+      }.to_json
+    end
+  end
+
+  def destroy
+    @post.destroy
+    render status: 200, json: {
+      message: "post deleted"
+    }.to_json
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :details)
+    end
+
+    def find_post
+      @post = Post.find(params[:id])
+    end
 end
