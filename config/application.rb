d6c21318b99b16a8f213834bdc63b5660d9f2842
Martin Ben <mebenav88@gmail.com>
2016-05-23 15:59:53 -0500

    add custom font- test trial

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module RailsAngularAuthenticationProject
     config.active_record.raise_in_transactional_callbacks = true
 
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+    # config.assets.paths << Rails.root.join('/app/assets/fonts')
   end
 end
